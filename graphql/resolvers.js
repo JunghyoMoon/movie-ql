61e3d45f30d16707333fd83f278a164508b61bc5
JungHyoMoon <dsinhome70@gmail.com>
2021-08-14 23:04:31 +0900

    graphql's basic movement

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { people } from "./db";
+import { people, getById } from "./db";
 
 const Moon = {
     name: "junghyo",
@@ -6,10 +6,18 @@ const Moon = {
     gender: "male",
 };
 
+/*
+graphql이란 무엇인가?
+
+query(질문, operation?) 에 대한 resolve(해결)
+데이터가 어떤 형태여야 하는지 schema로 정하고, resolver로 정해진 형식의 요청을 전달?
+
+실제 데이터베이스에 접근하는 건 당연히 가능하고, api에 접근하는 것도 가능함.
+*/
 const resolvers = {
     Query: {
         people: () => people,
-        person: () => Moon,
+        person: (_, { id }) => getById(id),
     },
 };
 
